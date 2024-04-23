14322544b15617a8b379125976fa9420a393f16a
Robson Roberto Souza Peixoto <robsonpeixoto@gmail.com>
2024-04-23 16:21:47 -0300

    feat: add simples hello world route

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/robsonpeixoto/learn-huma
 
 go 1.22.2
+
+require github.com/danielgtaylor/huma/v2 v2.14.0
+
+require (
+	github.com/fxamacker/cbor/v2 v2.6.0 // indirect
+	github.com/x448/float16 v0.8.4 // indirect
+)
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/danielgtaylor/huma/v2"
+
+	"github.com/danielgtaylor/huma/v2/adapters/humago"
+	_ "github.com/danielgtaylor/huma/v2/formats/cbor"
+)
+
+type GreetingInput struct {
+	Name string `path:"name" maxLength:"30" example:"world" doc:"Name to greet"`
+}
+
+// GreetingOutput represents the greeting operation response.
+type GreetingOutput struct {
+	Body struct {
+		Message string `json:"message" example:"Hello, world!" doc:"Greeting message"`
+	}
+}
+
+func main() {
+	// Create a new router & API
+	router := http.NewServeMux()
+
+	api := humago.New(router, huma.DefaultConfig("My API", "1.0.0"))
+
+	// Register GET /greeting/{name} handler.
+
+	huma.Register(api, huma.Operation{
+		OperationID: "get-greeting",
+		Method:      http.MethodGet,
+		Path:        "/greeting/{name}",
+		Summary:     "Get a greeting",
+		Description: "Get a greeting for a person by name.",
+		Tags:        []string{"Greetings"},
+	}, func(ctx context.Context, input *GreetingInput) (*GreetingOutput, error) {
+		resp := &GreetingOutput{}
+		resp.Body.Message = fmt.Sprintf("Hello, %s!", input.Name)
+		return resp, nil
+	})
+
+	// Start the server!
+	http.ListenAndServe("127.0.0.1:8888", router)
+}
